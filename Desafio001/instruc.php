952a4fbbab95a463cac0a1b66b2edc27ad3d67e0
Eider Xavier <eider1991@yahoo.com.br>
2023-12-13 16:18:23 -0300

    Alteração na exibição.
    Vc pode usar variáveis mas só vai gastar memória...

--- Desafio001/instruc.php
+++ Desafio001/instruc.php
@@ -7,11 +7,21 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <h1>Resultado Final</h1>
-    
     <header>
-
+    <h1>Resultado Final</h1>
     </header>
+    <main>
+        <?php 
+        $numero = $_GET["numero"] ?? "escreva um número!"; // ?? operador de coalescencia nula
+        echo "O número escolhido foi <strong>$numero</strong>";
+        echo "<p>O seu sucessor é " . $numero+1;
+        echo "<p>O seu antecessor é " . $numero-1;
+        ?>
+        <p>
+            <button onclick="javascript:window.location.href='index.html'">&#x2B05;Voltar</button>
+        </p>
+    </main>
+    
     
 </body>
 </html>
